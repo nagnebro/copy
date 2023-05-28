878dd5b6779ba578b7677020d175ef506caf38fb
Kim jisoo <ishnn032n@gmail.com>
2023-05-28 11:23:35 +0900

    fix: renscene 머지 오류 수정

--- ui/scene/renscene.py
+++ ui/scene/renscene.py
@@ -5,12 +5,12 @@ from ui.component.renconversation import RenConversation
 
 class RenScene(Entity):
 
-    def __init__(self, variables_object=None): # variables_object = None이라는 것은 매개변수값으로 아무것도 들어오지 않을시
-                                                # default값으로 none을 집어넣겠다는 것.
+    def __init__(self, image, background, variables_object=None):   # variables_object = None이라는 것은 매개변수값으로 아무것도 들어오지 않을시
+                                                                    # default값으로 none을 집어넣겠다는 것.
         super().__init__(parent=camera.ui)
 
         conversation = RenConversation(variables_object=variables_object, parent=self)
-        with open('_resources/script/test.txt' + variables_object.script, 'r', encoding='UTF8') as file:
+        with open('_resources/script/' + variables_object.script, 'r', encoding='UTF8') as file:
             # open(파일 경로, r/w, 인코딩)
 
             data = file.read()
